7056b11363e83fda3cc7aa3d6e87e5a4b28fb23c
Ibraheem-Mustafa <ibraheemmustafacontact@gmail.com>
2026-02-23 02:22:42 +0000

    feat(extensions): create custom-spacing extension with utility classes

--- plugins/sgs-blocks/src/blocks/extensions/index.js
+++ plugins/sgs-blocks/src/blocks/extensions/index.js
@@ -5,7 +5,10 @@
  *
  * - Animation: controls for sgs/* blocks only (scroll-triggered animations).
  * - Device visibility: show/hide per breakpoint for ALL blocks (core + SGS).
+ * - Hover effects: hover animations for sgs/* blocks.
+ * - Custom spacing: enhanced spacing controls for sgs/* blocks.
  */
 import './animation';
 import './responsive-visibility';
 import './hover-effects';
+import './custom-spacing';
